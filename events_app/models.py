4c9b7422b7b8ff0271b853db65cd0ff3ab0e6bb2
el634dev <al7758066@gmail.com>
2024-02-08 15:51:06 -0500

    Completed models and todos inside of models.py

--- events_app/models.py
+++ events_app/models.py
@@ -1,32 +1,45 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+from sqlalchemy import Column, DateTime, Integer, String
 
-# TODO: Create a model called `Guest` with the following fields:
-# - id: primary key
-# - name: String column
-# - email: String column
-# - phone: String column
-# - events_attending: relationship to "Event" table with a secondary table
-
+# Create a model called `Guest` with the following fields:
 class Guest(db.Model):
+    """Guest Model"""
+    # - id: primary key
     id = db.Column(db.Integer, primary_key=True)
+    # - name: String column
+    name = db.Column(db.String(80), nullable=False, unique=True)
+    # - email: String column
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    # - phone: String column
+    phone = db.Column(db.String(40), nullable=False, unique=True)
+    # - events_attending: relationship to "Event" table with a secondary table
+    events_attending = db.relationship("Event", secondary="events")
 
-# TODO: Create a model called `Event` with the following fields:
-# - id: primary key
-# - title: String column
-# - description: String column
-# - date_and_time: DateTime column
-# - guests: relationship to "Guest" table with a secondary table
+
+# -----------------------------------
+# Create a model called `Event` with the following fields:
+class Event(db.Model):
+    """Event Model"""
+    # - id: primary key
+    id = db.Column(db.Integer, primary_key=True)
+    # - title: String column
+    title = db.Column(db.String(40), nullable=False)
+    # - description: String column  
+    description = db.Column(db.String(80), nullable=False)
+    # - date_and_time: DateTime column
+    date_and_time = db.Column(DateTime)
+    # - guests: relationship to "Guest" table with a secondary table
+    guest = db.relationship("Guest", secondary="guests")
 
 # STRETCH CHALLENGE: Add a field `event_type` as an Enum column that denotes the
 # type of event (Party, Study, Networking, etc)
 
-class Event(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-
-# TODO: Create a table `guest_event_table` with the following columns:
-# - event_id: Integer column (foreign key)
-# - guest_id: Integer column (foreign key)
-
-guest_event_table = None
+# Create a table `guest_event_table` with the following columns:
+guest_event_table = db.Table('guest_event',
+        # - event_id: Integer column (foreign key)
+        db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+        # - guest_id: Integer column (foreign key)
+        db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
+)
